5b8f3129c5acd4b023ea6a800b822ae051b495d4
Nikola <nikiporumbachanov@gmail.com>
2022-02-18 15:29:49 +0200

    ProductScreen styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
@@ -14,7 +14,7 @@ function App() {
 				<Container>
 					<Routes>
 						<Route path='/' element={<HomeScreen />} />
-						<Route path='/product:id' element={<ProductScreen />} />
+						<Route path='/product/:id' element={<ProductScreen />} />
 					</Routes>
 				</Container>
 			</main>
