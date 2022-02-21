6c509235bbb5b10bde085e333d580243d367101c
Nikola <nikiporumbachanov@gmail.com>
2022-02-21 16:54:27 +0200

    add data seeder script

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const reviewSchema = mongoose.Schema.create(
+const reviewSchema = mongoose.Schema(
 	{
 		name: {type: String, required: true},
 		rating: {type: Number, required: true},
